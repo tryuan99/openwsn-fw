7bd368acf8a7f6014655058efbe378dbe40dc33b
Titan Yuan <tryuan99@gmail.com>
2025-01-17 17:36:38 -0800

    Tune each channel independently after calibration

--- bsp/boards/scum/tuning_feedback.c
+++ bsp/boards/scum/tuning_feedback.c
@@ -2,7 +2,9 @@
 
 #include <stdint.h>
 
+#include "IEEE802154E.h"
 #include "channel.h"
+#include "memory_map.h"
 #include "opendefs.h"
 #include "tuning.h"
 
@@ -21,57 +23,97 @@
 #define TUNING_FEEDBACK_MIN_NUM_IF_ESTIMATES_TO_AVERAGE \
     (TUNING_FEEDBACK_NUM_IF_ESTIMATES_TO_AVERAGE / 3)
 
-// Array of the latest IF estimates.
-static uint32_t
-    g_tuning_feedback_if_estimates[TUNING_FEEDBACK_NUM_IF_ESTIMATES_TO_AVERAGE];
+// Channel information.
+typedef struct {
+    // Array of the latest IF estimates.
+    uint32_t if_estimates[TUNING_FEEDBACK_NUM_IF_ESTIMATES_TO_AVERAGE];
 
-// If true, the entire IF estimates array is valid.
-static bool g_tuning_feedback_if_estimates_full_array = FALSE;
+    // If true, the entire IF estimates array is valid.
+    bool if_estimates_full_array;
 
-// Index for the next IF estimate.
-static uint32_t g_tuning_feedback_if_estimate_index = 0;
+    // Index for the next IF estimate.
+    size_t if_estimate_index;
+} tuning_feedback_channel_info_t;
+
+// Channel information for all channels.
+static tuning_feedback_channel_info_t
+    g_tuning_feedback_channel_infos[NUM_CHANNELS];
 
 // Increment the IF estimate index.
-static inline void tuning_feedback_increment_if_estimate_index(void) {
-    g_tuning_feedback_if_estimate_index =
-        (g_tuning_feedback_if_estimate_index + 1) %
+static inline void tuning_feedback_increment_if_estimate_index(
+    const uint8_t channel) {
+    const uint8_t channel_index = channel_convert_channel_to_index(channel);
+    g_tuning_feedback_channel_infos[channel_index].if_estimate_index =
+        (g_tuning_feedback_channel_infos[channel_index].if_estimate_index + 1) %
         TUNING_FEEDBACK_NUM_IF_ESTIMATES_TO_AVERAGE;
-    if (g_tuning_feedback_if_estimate_index == 0) {
-        g_tuning_feedback_if_estimates_full_array = TRUE;
+    if (g_tuning_feedback_channel_infos[channel_index].if_estimate_index == 0) {
+        g_tuning_feedback_channel_infos[channel_index].if_estimates_full_array =
+            TRUE;
     }
 }
 
 // Write the latest IF estimate to the array.
 static inline void tuning_feedback_write_if_estimate(
-    const uint32_t if_estimate) {
-    g_tuning_feedback_if_estimates[g_tuning_feedback_if_estimate_index] =
+    const uint8_t channel, const uint32_t if_estimate) {
+    const uint8_t channel_index = channel_convert_channel_to_index(channel);
+    const uint32_t index =
+        g_tuning_feedback_channel_infos[channel_index].if_estimate_index;
+    g_tuning_feedback_channel_infos[channel_index].if_estimates[index] =
         if_estimate;
-    tuning_feedback_increment_if_estimate_index();
+    tuning_feedback_increment_if_estimate_index(channel);
 }
 
 // Get the number of IF estimates in the array.
-static inline size_t tuning_feedback_num_if_estimates(void) {
-    return (g_tuning_feedback_if_estimates_full_array == TRUE)
+static inline size_t tuning_feedback_num_if_estimates(const uint8_t channel) {
+    const uint8_t channel_index = channel_convert_channel_to_index(channel);
+    return (g_tuning_feedback_channel_infos[channel_index]
+                .if_estimates_full_array == TRUE)
                ? TUNING_FEEDBACK_NUM_IF_ESTIMATES_TO_AVERAGE
-               : g_tuning_feedback_if_estimate_index;
+               : g_tuning_feedback_channel_infos[channel_index]
+                     .if_estimate_index;
 }
 
 // Average the IF estimates.
-static inline uint32_t tuning_feedback_average_if_estimates(void) {
+static inline uint32_t tuning_feedback_average_if_estimates(
+    const uint8_t channel) {
+    const uint8_t channel_index = channel_convert_channel_to_index(channel);
     const size_t num_if_estimates_to_average =
-        tuning_feedback_num_if_estimates();
+        tuning_feedback_num_if_estimates(channel);
     uint32_t if_estimate_sum = 0;
     uint32_t i = 0;
     for (i = 0; i < num_if_estimates_to_average; ++i) {
-        if_estimate_sum += g_tuning_feedback_if_estimates[i];
+        if_estimate_sum +=
+            g_tuning_feedback_channel_infos[channel_index].if_estimates[i];
     }
     return if_estimate_sum / num_if_estimates_to_average;
 }
 
 // Reset the IF estimates.
-static inline void tuning_feedback_reset_if_estimates(void) {
-    g_tuning_feedback_if_estimates_full_array = FALSE;
-    g_tuning_feedback_if_estimate_index = 0;
+static inline void tuning_feedback_reset_if_estimates(const uint8_t channel) {
+    const uint8_t channel_index = channel_convert_channel_to_index(channel);
+    g_tuning_feedback_channel_infos[channel_index].if_estimates_full_array =
+        FALSE;
+    g_tuning_feedback_channel_infos[channel_index].if_estimate_index = 0;
+}
+
+// Print the tuning code feedback over UART.
+static inline void tuning_feedback_print_tuning_code(
+    const uint8_t channel, const tuning_code_t* tuning_code) {
+    UART_REG__TX_DATA = '~';
+    // Print the channel.
+    UART_REG__TX_DATA = '0' + channel / 10;
+    UART_REG__TX_DATA = '0' + channel % 10;
+    UART_REG__TX_DATA = ' ';
+    // Print the tuning code.
+    UART_REG__TX_DATA = '0' + tuning_code->coarse / 10;
+    UART_REG__TX_DATA = '0' + tuning_code->coarse % 10;
+    UART_REG__TX_DATA = '.';
+    UART_REG__TX_DATA = '0' + tuning_code->mid / 10;
+    UART_REG__TX_DATA = '0' + tuning_code->mid % 10;
+    UART_REG__TX_DATA = '.';
+    UART_REG__TX_DATA = '0' + tuning_code->fine / 10;
+    UART_REG__TX_DATA = '0' + tuning_code->fine % 10;
+    UART_REG__TX_DATA = '\n';
 }
 
 void tuning_feedback_adjust_rx(const uint8_t channel,
@@ -82,16 +124,17 @@ void tuning_feedback_adjust_rx(const uint8_t channel,
     }
 
     // Write the IF estimate.
-    tuning_feedback_write_if_estimate(if_estimate);
+    tuning_feedback_write_if_estimate(channel, if_estimate);
 
     // Only tune the tuning codes if there are sufficient IF estimates.
-    if (tuning_feedback_num_if_estimates() <
+    if (tuning_feedback_num_if_estimates(channel) <
         TUNING_FEEDBACK_MIN_NUM_IF_ESTIMATES_TO_AVERAGE) {
         return;
     }
 
     // Average the latest IF estimates.
-    const uint32_t if_estimate_average = tuning_feedback_average_if_estimates();
+    const uint32_t if_estimate_average =
+        tuning_feedback_average_if_estimates(channel);
 
     // Adjust the RX tuning code.
     tuning_code_t tuning_code;
@@ -102,17 +145,15 @@ void tuning_feedback_adjust_rx(const uint8_t channel,
         // TODO(titan): Handle fine code and mid code overflows.
         tuning_increment_fine_codes(&tuning_code, /*num_fine_codes=*/1);
         channel_set_tuning_code(channel, CHANNEL_MODE_RX, &tuning_code);
-        printf("Incrementing RX tuning code to %d.%d.%d.\n", tuning_code.coarse,
-               tuning_code.mid, tuning_code.fine);
-        tuning_feedback_reset_if_estimates();
+        tuning_feedback_print_tuning_code(channel, &tuning_code);
+        tuning_feedback_reset_if_estimates(channel);
     } else if (if_estimate_average < TUNING_FEEDBACK_NOMINAL_IF_COUNT -
                                          TUNING_FEEDBACK_MAX_IF_OFFSET) {
         // The IF estimate is too low.
         // TODO(titan): Handle fine code and mid code underflows.
         tuning_decrement_fine_codes(&tuning_code, /*num_fine_codes=*/1);
         channel_set_tuning_code(channel, CHANNEL_MODE_RX, &tuning_code);
-        printf("Decrementing RX tuning code to %d.%d.%d.\n", tuning_code.coarse,
-               tuning_code.mid, tuning_code.fine);
-        tuning_feedback_reset_if_estimates();
+        tuning_feedback_print_tuning_code(channel, &tuning_code);
+        tuning_feedback_reset_if_estimates(channel);
     }
 }
