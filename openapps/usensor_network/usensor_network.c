bcecdf0c77335012912d8e6c39aa93532800e873
Titan Yuan <tryuan99@gmail.com>
2025-03-10 12:00:22 -0700

    Add sensor readout for sensor network application

--- openapps/usensor_network/usensor_network.c
+++ openapps/usensor_network/usensor_network.c
@@ -4,6 +4,10 @@
 
 #include "usensor_network.h"
 
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+
 #include "IEEE802154E.h"
 #include "async.h"
 #include "icmpv6rpl.h"
@@ -17,18 +21,56 @@
 #include "scheduler.h"
 #include "sock.h"
 
+#ifdef SCUM
+#include "adc.h"
+#include "memory_map.h"
+#include "sensors.h"
+#include "tuning.h"
+#endif // SCUM
+
 // Sensor network packet period in milliseconds.
-#define USENSOR_NETWORK_PERIOD_MS 3000
+#define USENSOR_NETWORK_PERIOD_MS 2000
 
 // Sensor network traffic rate.
 #define USENSOR_NETWORK_TRAFFIC_RATE 1
 
 // Sensor network maximum buffer size in bytes.
-#define USENSOR_NETWORK_MAX_BUFFER_SIZE 50
+#define USENSOR_NETWORK_MAX_BUFFER_SIZE 64
+
+// Sensor network maximum number of sensors in the packet.
+#define USENSOR_NETWORK_MAX_NUM_SENSORS 8
 
 // Sensor payload.
 typedef struct __attribute__((packed)) {
-  uint16_t data;
+  // Sequence number.
+  uint8_t sequence_number;
+
+  // Channel. Unused for the sensor network application.
+  uint8_t channel;
+
+  // Reserved.
+  uint8_t reserved1;
+
+  // Reserved.
+  uint8_t reserved2;
+
+  // Measurement output.
+  uint32_t output[USENSOR_NETWORK_MAX_NUM_SENSORS];
+
+  // Tuning code. Unused for the sensor network application.
+#ifdef SCUM
+  tuning_code_t tuning_code;
+#else  // !SCUM
+uint8_t coarse;
+uint8_t mid;
+uint8_t fine;
+#endif // SCUM
+
+  // Reserved.
+  uint8_t reserved3;
+
+  // CRC. Unused for the sensor network application.
+  uint16_t crc;
 } sensor_network_payload_t;
 
 // Data packet.
@@ -40,11 +82,56 @@ typedef struct __attribute__((packed)) {
   sensor_network_payload_t payload;
 } sensor_network_packet_t;
 
+#ifdef SCUM
+// ADC configuration.
+static const adc_config_t g_sensor_network_adc_config = {
+    .reset_source = ADC_RESET_SOURCE_FSM,
+    .convert_source = ADC_CONVERT_SOURCE_FSM,
+    .pga_amplify_source = ADC_PGA_AMPLIFY_SOURCE_FSM,
+    .pga_gain = 0,
+    .settling_time = 0,
+    .bandgap_reference_tuning_code = 1,
+    .const_gm_tuning_code = 0xFF,
+    .vbat_div_4_enabled = FALSE,
+    .ldo_enabled = TRUE,
+    .input_mux_select = ADC_INPUT_MUX_SELECT_EXTERNAL_SIGNAL,
+    .pga_bypass = TRUE,
+};
+
+// Sensors configuration.
+static const sensors_config_t g_sensor_network_sensors_config = {
+  .gpio_strobe = GPIO_0,
+  .gpios_select =
+      {
+          GPIO_1,
+          GPIO_2,
+          GPIO_3,
+      },
+  .num_sensors = 5,
+  .sensors =
+      {
+          SENSOR_TYPE_POTENTIOMETRIC,
+          SENSOR_TYPE_POTENTIOMETRIC,
+          SENSOR_TYPE_POTENTIOMETRIC,
+          SENSOR_TYPE_POTENTIOMETRIC,
+          SENSOR_TYPE_POTENTIOMETRIC,
+      },
+  .sensor_configs =
+      {
+          {0},
+          {0},
+          {0},
+          {0},
+          {0},
+      },
+};
+#endif // SCUM
+
+// Packet sequence number.
+static uint8_t g_sensor_network_sequence_number = 0;
+
 // Data packet.
 static sensor_network_packet_t g_sensor_network_packet;
-
-// Counter.
-static uint16_t g_sensor_network_counter = 0;
 
 // Timer ID.
 static opentimers_id_t g_sensor_network_timer_id;
@@ -69,17 +156,26 @@ static void sensor_network_sock_handler(sock_udp_t *sock,
     int16_t res = 0;
     char buffer[USENSOR_NETWORK_MAX_BUFFER_SIZE];
     sock_udp_ep_t remote;
-    int i = 0;
     if ((res = sock_udp_recv(sock, buffer, USENSOR_NETWORK_MAX_BUFFER_SIZE, 0,
                              &remote)) >= 0) {
       openserial_printf("Received %d bytes from remote endpoint:\n", res);
       openserial_printf(" - port: %d", remote.port);
       openserial_printf(" - addr: ", remote.port);
-      for (i = 0; i < 16; ++i)
+      for (size_t i = 0; i < 16; ++i) {
         openserial_printf("%x ", remote.addr.ipv6[i]);
+      }
 
       openserial_printf("\n\n");
-      openserial_printf("Msg received: %s\n\n", buffer);
+      openserial_printf("Msg received: %s\n", buffer);
+
+      // Print out sensor data.
+      sensor_network_packet_t *received_packet =
+          (sensor_network_packet_t *)buffer;
+      openserial_printf("Sensor data:");
+      for (size_t i = 0; i < USENSOR_NETWORK_MAX_NUM_SENSORS; ++i) {
+        openserial_printf(" %u", received_packet->payload.output[i]);
+      }
+      openserial_printf("\n");
     }
   }
 
@@ -87,14 +183,20 @@ static void sensor_network_sock_handler(sock_udp_t *sock,
     owerror_t error = *(uint8_t *)arg;
     if (error == E_FAIL) {
       LOG_ERROR(COMPONENT_USENSOR_NETWORK, ERR_MAXRETRIES_REACHED,
-                (errorparameter_t)g_sensor_network_counter,
+                (errorparameter_t)g_sensor_network_sequence_number,
                 (errorparameter_t)0);
     }
+    printf("Sent sensor network packet\n");
     g_sensor_network_busy_sending = FALSE;
   }
 }
 
 static inline void sensor_network_task_cb(void) {
+#ifdef SCUM
+  UART_REG__TX_DATA = '%';
+  UART_REG__TX_DATA = '\n';
+#endif // SCUM
+
   // Check if synchronized.
   if (ieee154e_isSynch() == FALSE) {
     return;
@@ -124,6 +226,12 @@ static inline void sensor_network_task_cb(void) {
     return;
   }
 
+#ifdef SCUM
+  // Measure the sensors.
+  sensors_measurements_t sensor_measurements;
+  sensors_measure(&sensor_measurements);
+#endif // SCUM
+
   sock_udp_ep_t remote = (sock_udp_ep_t){
       .family = AF_INET6,
       .port = WKP_UDP_SENSOR_NETWORK,
@@ -133,13 +241,33 @@ static inline void sensor_network_task_cb(void) {
   memset(&g_sensor_network_packet, 0, sizeof(sensor_network_packet_t));
   memcpy(&g_sensor_network_packet.addr_16b,
          idmanager_getMyID(ADDR_16B)->addr_16b, sizeof(uint16_t));
-  g_sensor_network_packet.payload.data = g_sensor_network_counter;
+
+#ifdef SCUM
+  // Assemble the payload of the sensor network packet.
+  g_sensor_network_packet.payload.sequence_number =
+      g_sensor_network_sequence_number;
+  for (size_t i = 0; i < g_sensor_network_sensors_config.num_sensors; ++i) {
+    switch (g_sensor_network_sensors_config.sensors[i]) {
+    case SENSOR_TYPE_POTENTIOMETRIC: {
+      g_sensor_network_packet.payload.output[i] =
+          sensor_measurements.measurements[i].adc_output;
+      break;
+    }
+    case SENSOR_TYPE_RESISTIVE:
+    case SENSOR_TYPE_PH:
+    case SENSOR_TYPE_INVALID:
+    default: {
+      break;
+    }
+    }
+  }
+#endif // SCUM
 
   if (sock_udp_send(&g_sensor_network_sock, &g_sensor_network_packet,
                     sizeof(sensor_network_packet_t), &remote) > 0) {
     g_sensor_network_busy_sending = TRUE;
   }
-  ++g_sensor_network_counter;
+  ++g_sensor_network_sequence_number;
 }
 
 static void sensor_network_timer_cb(opentimers_id_t id) {
@@ -165,6 +293,15 @@ void usensor_network_init(void) {
       "Created a UDP socket for the sensor network application.\n");
 
   sock_udp_set_cb(&g_sensor_network_sock, sensor_network_sock_handler, NULL);
+
+#ifdef SCUM
+  // Configure the ADC.
+  adc_config(&g_sensor_network_adc_config);
+  adc_enable_interrupt();
+
+  // Configure the sensors.
+  sensors_init(&g_sensor_network_sensors_config);
+#endif // SCUM
 
   // Start a periodic timer.
   g_sensor_network_timer_id =
